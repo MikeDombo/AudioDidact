778297f6212aa0c34b791d81e3cf09bf28e132e1
Michael Dombrowski <Michael@mikedombrowski.com>
2016-09-03 22:45:45 -0400

    Add basic homepage for unauthenticated users

--- index.php
+++ index.php
@@ -20,7 +20,26 @@ require_once(__DIR__."/views/views.php");
 				</html>";
 				exit(0);
 			}?>
-			
+			<div class="col-md-4">
+				<h3>What is PodTube?</h3>
+				<hr/>
+				<p>PodTube means taking YouTube on the go and sharing your favorite videos with friends.</p>
+			</div>
+			<div class="col-md-4">
+				<h3>How Does This Work?</h3>
+				<hr/>
+				<h4><a href="signup.php">Sign up for an account</a> to get started.</h4>
+				<p>Enter any YouTube video URL or video ID in the textbox and click "Add Video To Feed" to put the
+				video into the created podcast feed. Then subscribe to the feed URL shown on the "Add Video" page
+				with any podcatcher.</p>
+			</div>
+			<div class="col-md-4">
+				<h3>Development</h3>
+				<hr/>
+				<p>PodTube is being developed by <a href="http://mikedombrowski.com" target="_blank">Michael Dombrowski</a>
+				 and the source is available on my <a href="http://github.com/md100play/PodTube" target="_blank">GitHub</a>.
+				 Documentation is online <a href="http://md100play.github.io/PodTube" target="_blank">here</a>.</p>
+			</div>
 		</div>
 	</body>
 </html>
